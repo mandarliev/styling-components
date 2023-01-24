42cf6f658de96e0e7485087871d4c75dc17a4499
Dimitar <dimitar.mandarliev@gmail.com>
2023-01-24 14:37:53 +0200

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Little Lemon Restaurant</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F4CE14',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    padding: 24,
+    marginTop: 16,
+    paddingVertical: 1,
+    borderWidth: 2,
+    borderColor: '#495E57',
+    borderRadius: 4,
+    backgroundColor: '#495E57',
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
 });
